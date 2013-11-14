54dd5d728ddbdb98088bd794038318074c1efe2c
iori <matsuhar@gmail.com>
2013-11-14 22:35:48 +0100

    colors are memoized in the controller now

--- app/models/section.rb
+++ app/models/section.rb
@@ -14,7 +14,6 @@ class Section
     @header = header
     @main_article = main_article
     @articles = articles
-    @colors = ["#1f0d67", "#fe2f2f", "#d50303", "#6faa12", "#006169", "#30932e", "#f20559", "#0cb4ae", "#ff6e17", "#1f0d67", "#fe2f2f", "#d50303", "#6faa12", "#006169", "#30932e", "#f20559", "#0cb4ae", "#ff6e17"]
   end
 
   def self.lasts
