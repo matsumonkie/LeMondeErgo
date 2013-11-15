04d12de380fc9b2e9e77be1d9016a0826210cb7f
iori <matsuhar@gmail.com>
2013-11-15 10:25:20 +0100

    color is now a model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   
   def index
     @sections = Page.last
-    @colors ||= ["#1f0d67", "#fe2f2f", "#d50303", "#6faa12", "#006169", "#30932e", "#f20559", "#0cb4ae", "#ff6e17", "#1f0d67", "#fe2f2f", "#d50303", "#6faa12", "#006169", "#30932e", "#f20559", "#0cb4ae", "#ff6e17"]
+    @colors ||= Color.all
   end
   
 end
---
+++ app/models/color.rb
@@ -0,0 +1,10 @@
+class Color
+
+  @@colors = %w[#1f0d67 #fe2f2f #d50303 #6faa12 #006169 #30932e #f20559 #0cb4ae #ff6e17
+                #1f0d67 #fe2f2f #d50303 #6faa12 #006169 #30932e #f20559 #0cb4ae #ff6e17]
+
+  def self.all()
+    return @@colors
+  end  
+
+end
