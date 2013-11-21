d30ed41a7fa6500c5ec6561f42db1acb91fcdfdf
iori <matsuhar@gmail.com>
2013-11-21 11:14:33 +0100

    remove useless function in util

--- app/models/util.rb
+++ app/models/util.rb
@@ -1,18 +1,11 @@
 class Util
   
   def self.addWebsiteURLIfMissing(url)
-    url = if (url.start_with?("/"))
+    if (url.start_with?("/"))
       LeMondeErgo::Application::LE_MONDE_SITE_URL + url
     else
       url
-    end
-    
-    return url
-  end
-
-  # [1,2,3,4] should become [[1,2], [3,4]]
-  def self.joinArrayByPair(array)
-    array.each_slice(2).to_a
+    end       
   end
 
 end
