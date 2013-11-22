d7d983ceb1000428b25602d4dd9311a3a801ff8f
iori <matsuhar@gmail.com>
2013-11-22 14:06:05 +0100

    remove secret token from .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeMondeErgo::Application.config.secret_key_base = 'da94b6edc8daca0a310eb0fca9cd6acfad9a58a1d2c7e92271013814128d3051e7c4cb186c207b6b9de97659c763ad7a502e4ed38cac9f4c7e539ae29d70cfce'
